43ec2f494025ce932bd692c9687d332d791db40f
Rupeshsharma12 <143415103+Rupeshsharma12@users.noreply.github.com>
2023-08-28 20:06:01 +0530

    First Commit Program 1 using C

---
+++ Chapter01/Program001.c
@@ -0,0 +1,9 @@
+// 1 Write A Program to Print Hello World.
+#include<stdio.h>
+
+int main(){
+    printf("Hello World, Lets start to Learn C Language");
+    printf("\nThis is My 1st Program");
+    
+    return 0;
+}
